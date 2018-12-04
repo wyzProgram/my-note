11463befc2a0f66d2afedd7a6e681b12a2249be6
858272689 <858272689@qq.com>
2018-12-04 15:24:20 +0800

    update

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+//
+// 有关程序集的常规信息通过下列属性集
+// 控制。更改这些属性值可修改
+// 与程序集关联的信息。
+//
+[assembly: AssemblyTitle("MyNote")]
+[assembly: AssemblyDescription("多功能记事本")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("百联百软件")]
+[assembly: AssemblyProduct("MyNote")]
+[assembly: AssemblyCopyright("BaiLianBai")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("f4ee7ce8-97f1-4533-827f-18f07cc89006")]
+
+//
+// 程序集的版本信息由下面四个值组成:
+//
+//      主版本
+//      次版本
+//      内部版本号
+//      修订号
+//
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用“*”:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("zh-CHS")]
